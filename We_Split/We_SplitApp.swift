42c82ba90835e801904f9002a6cea16d49964483
bopatynski <bopatynski@gmail.com>
2022-04-19 01:35:27 -0400

    Initial Commit

---
+++ We_Split/We_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  We_SplitApp.swift
+//  We_Split
+//
+//  Created by Bogdan Patynski on 2022-04-19.
+//
+
+import SwiftUI
+
+@main
+struct We_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
